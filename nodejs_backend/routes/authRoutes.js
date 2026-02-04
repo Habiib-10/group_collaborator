4c403017129609100f9416d8bfe9e2fa19839dcf
Habiib-10 <dameerburiye@gmail.com>
2026-02-04 09:38:22 +0300

    Team Setup: Folders lib, nodejsbackend and server.js added

--- nodejs_backend/routes/authRoutes.js
+++ nodejs_backend/routes/authRoutes.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController'); // Hubi in kani jiro
-
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
+//halkaan soo dhig koodhkaaga Dart ee faylka routes/authRoutes.dart
--- nodejs_backend/routes/cropRoutes.js
+++ nodejs_backend/routes/cropRoutes.js
@@ -1,11 +1,2 @@
 
-const express = require('express');
-const router = express.Router();
-const cropController = require('../controllers/cropController');
-const auth = require('../middleware/authMiddleware');
-
-router.get('/', cropController.getCrops);
-router.post('/', auth, cropController.addCrop); 
-router.delete('/:id', auth, cropController.deleteCrop);
-
-module.exports = router;
+//halkaan soo dhig koodhkaaga Dart ee faylka routes/cropRoutes.dart
